ac9ee54b0c985ba6709a895069d66099e234e93e
MansurAhmed1 <mansurahmed75236@gmail.com>
2022-02-17 23:09:37 +0600

    comment

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ function innerText(balance) {
 document
   .querySelector(".calculate-button")
   .addEventListener("click", function () {
+
     var incomeInputValue = allInput(".income-input-feild");
     var foodInputValue = allInput(".food-input-feild");
     var rentInputValue = allInput(".rent-input-feild");
@@ -52,10 +53,10 @@ document
 
       balancemoney.innerText = balance;
     }
-else{
-  balancemoney.innerText=0;
-      expensesMoney.innerText=0;
-}
+// else{
+//   balancemoney.innerText=0;
+//       expensesMoney.innerText=0;
+// }
 // if(balance < 0 || )
 
     if (balance < 0) {
@@ -71,10 +72,10 @@ else{
     
     }
 
-    if(incomeInputValue < 0 ||foodInputValue<0 || rentInputValue<0 || clothesInputValue <0){
-      balancemoney.innerText=0;
-      expensesMoney.innerText=0;
-    }
+    // if(incomeInputValue < 0 ||foodInputValue<0 || rentInputValue<0 || clothesInputValue <0){
+    //   balancemoney.innerText=0;
+    //   expensesMoney.innerText=0;
+    // }
 
     if (incomeInputValue < 0) {
       incomeError.innerHTML = `<span> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
@@ -159,7 +160,8 @@ document.querySelector(".save-button ").addEventListener("click", function () {
 
     } else if (saveamount > balanceMoneyValue ) {
       // saveAmount.innerText =0;
-      // remainingBalnce.innerText=0;
+      //  remainingBalnce.innerText=0;
+      remainingBalnce.innerText = parseFloat(balanceMoneyValue) - saveamount;
       savingError.innerHTML = ` <span> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
     <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
   </svg></span> saving money getter than balance money!!`;
@@ -169,8 +171,8 @@ document.querySelector(".save-button ").addEventListener("click", function () {
  
   }
   
-  if(saveamount > balanceMoneyValue || saveParcentageValue =="" || saveParcentageValue<0 || isNaN(saveParcentageValue)){
-    saveAmount.innerText =0;
-    remainingBalnce.innerText=0;
-  }
+  // if(saveamount > balanceMoneyValue || saveParcentageValue =="" || saveParcentageValue<0 || isNaN(saveParcentageValue)){
+  //   saveAmount.innerText =0;
+  //   remainingBalnce.innerText=0;
+  // }
 });

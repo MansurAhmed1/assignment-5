69174ad8ee21ac08069c5320cee08f7b0ab5971b
MansurAhmed1 <mansurahmed75236@gmail.com>
2022-02-17 17:29:57 +0600

    seven

--- index.js
+++ index.js
@@ -116,14 +116,20 @@ document.querySelector(".save-button ").addEventListener("click", function () {
   var incomeInputValue = allInput(".income-input-feild");
 
   var remainingBalnce = innerText(".ramining-balance");
+  
   var savingError = innerText(".saving-error");
+  var saveAmount=innerText(".saving-amount");
   // save amount
   var saveamount =
     parseFloat(incomeInputValue) * (parseFloat(saveParcentageValue) / 100);
   //  all condition
   if (saveParcentageValue > 0) {
     //    set save amount
-    document.querySelector(".saving-amount").innerText = saveamount;
+    saveAmount.innerText = saveamount;
+ 
+  
+
+
     var balanceMoney = innerText(".balance-money");
     var balanceMoneyValue = balanceMoney.innerText;
 
@@ -131,7 +137,10 @@ document.querySelector(".save-button ").addEventListener("click", function () {
       // remaining balance
       remainingBalnce.innerText = parseFloat(balanceMoneyValue) - saveamount;
       savingError.innerHTML = "";
+
     } else if (saveamount > balanceMoneyValue) {
+      saveAmount.innerText =0;
+      remainingBalnce.innerText=0;
       savingError.innerHTML = ` <span> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
     <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
   </svg></span> saving amount getter than balance money!!`;

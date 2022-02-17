5f653279dc713a80143ac2f06ed83a3d7e4086a0
MansurAhmed1 <mansurahmed75236@gmail.com>
2022-02-17 18:19:51 +0600

    twelve

--- index.js
+++ index.js
@@ -56,7 +56,12 @@ document
       balanceError.innerHTML = `<span> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
         <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
       </svg></span> expense is getter than income`;
-    } else balanceError.innerText = "";
+      balancemoney.innerText=0;
+      expensesMoney.innerText=0;
+    } else{
+      balanceError.innerText = '';
+    
+    }
 
     if (incomeInputValue < 0) {
       incomeError.innerHTML = `<span> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
@@ -106,6 +111,7 @@ document
     <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
   </svg></span> please enter number value to Clothes input!!`;
     }
+   
   });
 
 // Save button click
